433abd360259d5c15363a99c124891a936fe27fc
HuntOreo <hloreair@outlook.com>
2025-09-14 00:37:45 -0400

    Grab all menu btns

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import Menu from './components/Menu.js';
 import About from './components/About.js';
 import { render } from './Render.js';
 
-const homeBtn = document.querySelector('#home');
-const menuBtn = document.querySelector('#menu');
-const aboutBtn = document.querySelector('#about');
+const buttons = document.querySelectorAll('header button');
+const homeBtn = buttons[0];
+const menuBtn = buttons[1];
+const aboutBtn = buttons[2];
 
 homeBtn.addEventListener('click', () => render(Home));
 menuBtn.addEventListener('click', () => render(Menu));
 aboutBtn.addEventListener('click', () => render(About));
 
-render(Home);
+function toggleActivePage() {
+  for (let btn of buttons) {
+    btn.classList.toggle();
+  }
+}
+render(Home); 
